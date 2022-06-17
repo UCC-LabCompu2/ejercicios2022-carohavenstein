4f5366ab5ef7c9574ad88014e878b5484b5ee0f3
carohavenstein <carohavenstein@gmail.com>
2022-06-17 19:13:11 -0300

    se agregaron ejercicios canvas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -120,3 +120,92 @@ function cargarResultado(){
     document.getElementById(elementid: "dist").value = can + " " + un;
 }
 
+
+function dibujar_circulo_cuadrado(){
+    let canvas = document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+
+    let yMax = canvas.height;
+    let xMax = canvas.width;
+    let margen = 10;
+    let tamCuadrado = 50;
+
+    ctx.fillRect(0+margen, yMax-50-margen, tamCuadrado, tamCuadrado);
+
+    ctx.arc(xMax/2, yMax/2,100, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = "#00ff00";
+    ctx.fill();
+
+}
+
+function borrarCanvas(){
+    let canvas = document.getElementById("lienzoDibujo");
+    let ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+}
+
+function cargarListener(){
+    document.getElementById("lienzoDibujo").addEventListener("mousemove", function(event){
+        let canvas = document.getElementById("lienzoDibujo");
+        let ctx = canvas.getContext("2d");
+
+        let mouseX = event.clientX;
+        let mouseY = event.clientY;
+
+        console.log(mouseX, mouseY);
+
+        canvas.onmousedown = function(){bandera = true};
+        canvas.onmouseup = function(){bandera = false};
+        if(bandera){
+            ctx.fillRect(mouseX-10, mouseY-10, 5, 5);
+        }
+
+    });
+}
+
+function dibujarCuadriculado(){
+    let canvas = document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+
+    //Dibujar lineas horozontales
+    for(let i = 0; i < canvas.height; i += 20) {
+        ctx.beginPath();
+        ctx.moveTo(0, i);
+        ctx.lineTo(1000, i);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    //Dibujar lineas verticales
+    for(let i = 0; i < canvas.width; i += 20) {
+        ctx.beginPath();
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, canvas.height);
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
+
+x = 0;
+dx = 5;
+function animarAuto(){
+    let canvas = document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+    let img;
+
+    canvas.width = canvas.width;
+
+    img = new Image();
+    img.src = "images/auto.png";
+
+    img.onload = function(){
+        ctx.drawImage(img, x, 100);
+    }
+
+    if(x > canvas.width){
+        x = 0;
+    }
+    x = x+dx
+}
